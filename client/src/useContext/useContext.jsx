0fb6bc7183ddb1b391b5fc52d46ec790704fe8ae
Raj maddheshiya <rajmaddheshiya-kumarmaddeshiyaraj@gmail.com>
2026-04-06 12:13:47 +0530

    Fix: removed trailing slash from server URL

--- client/src/useContext/useContext.jsx
+++ client/src/useContext/useContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from "react";
 export const dataContext = createContext()
 function UserContext({ children }) {
-    const serverUrl = "https://codewithpragati-api.onrender.com/"
+    const serverUrl = "https://codewithpragati-api.onrender.com"
     // const nbn = "http://localhost:8000"
     const value = {
         serverUrl
