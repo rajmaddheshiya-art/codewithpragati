7d8b464e22e066b8800bb636791a67698aae1932
Raj maddheshiya <rajmaddheshiya-kumarmaddeshiyaraj@gmail.com>
2026-04-06 11:54:44 +0530

    Fix: css import error in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './app.css'
+import './App.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router"
 import UserContext from './useContext/useContext.jsx'
